6356a190b8ee3dde16ab67f256908e811f73925b
Keijiro Takahashi <keijiro@Super-MSX.local>
2013-11-15 19:09:09 +0900

    Initial Commit

---
+++ GLVX/GLVXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GLVXAppDelegate.h
+//  GLVX
+//
+//  Created by Keijiro Takahashi on 11/15/13.
+//  Copyright (c) 2013 Keijiro Takahashi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GLVXAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ GLVX/GLVXAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  GLVXAppDelegate.m
+//  GLVX
+//
+//  Created by Keijiro Takahashi on 11/15/13.
+//  Copyright (c) 2013 Keijiro Takahashi. All rights reserved.
+//
+
+#import "GLVXAppDelegate.h"
+
+@implementation GLVXAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
